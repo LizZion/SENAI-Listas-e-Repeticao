8aea0d9e1b35b5ea89ebb03f034106afff0cf512
unknown <lizzbentle@gmail.com>
2023-04-14 11:23:35 -0300

    Exercício 5 Completo!

--- ExercícioCinco.py
+++ ExercícioCinco.py
@@ -2,10 +2,16 @@ print("Insira a população do país A:")
 pop_A = float(input())
 print("Insira o crescimento do país A:")
 cresc_A = (pop_A * float(input())) / 100
+while cresc_A < 0:
+    print("Crescimento inválido, não pode ser negativo!")
+    cresc_A = (pop_A * float(input())) / 100
 print("Insira a população do país B:")
 pop_B = float(input()) 
 print("Insira o crescimento do país B:")
 cresc_B = (pop_B * float(input())) / 100
+while cresc_B < 0:
+    print("Crescimento inválido, não pode ser negativo!")
+    cresc_B = (pop_B * float(input())) / 100
 anos = 0
 while pop_A < pop_B:
     pop_A = pop_A + cresc_A
