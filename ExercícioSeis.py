c861524ec3e86632b7fed1770940ebcfffaca400
unknown <lizzbentle@gmail.com>
2023-04-14 14:28:10 -0300

    Exercício 6 completo!

---
+++ ExercícioSeis.py
@@ -0,0 +1,13 @@
+#VERSÃO 1
+numero = 0
+while numero < 20:
+    numero += 1
+    print(numero) 
+
+#VERSÃO 2
+numero = 0
+final = []
+while numero < 20:
+    numero += 1
+    final.append(numero)
+print(final) 
